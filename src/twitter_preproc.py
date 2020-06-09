bf225512cc80ccedb654e892ed6aad5e7a9e1838
oStritze <oliver.stritzel@googlemail.com>
2020-06-09 17:38:01 +0200

    added function for MF

--- src/twitter_preproc.py
+++ src/twitter_preproc.py
@@ -16,28 +16,47 @@ class twitter_preproc:
     def getDF(self):
         return self.outputDF
     
+    def _preprocessMF(self):
+        outputDF = self.inputData
+        
+        outputDF = outputDF.select(["tweet_id","engaged_user_id","engaged_with_user_id",
+                                    "retweet_timestamp","reply_timestamp",
+                                    "retweet_with_comment_timestamp","like_timestamp"])
+        return outputDF
+        
+        
+    
     def _preprocess(self, seed):
         from pyspark.ml.feature import RegexTokenizer, OneHotEncoderEstimator, StringIndexer
         
+        outputDF = self.inputData
+        
+        # Drop unnecessary cols
+        ### drop ids for classification
+        outputDF = outputDF.drop("tweet_id").drop("engaged_user_id").drop("engaged_with_user_id")
+        #
+        
         # Split the text tokens to valid format
         regexTokenizer = RegexTokenizer(inputCol="text_tokens",outputCol="vector", pattern="\t")
-        outputDF = regexTokenizer.transform(self.inputData)
+        outputDF = regexTokenizer.transform(outputDF)
         outputDF = outputDF.drop("text_tokens").withColumnRenamed("vector", "text_tokens")
         
+        #regexTokenizer = RegexTokenizer(inputCol="present_media", outputCol="media_list")
+        #outputDF = regexTokenizer.transform(regexTokenizer)
+        #outputDF = outputDF.drop("present_media").withColumnRenamed("vector", "text_tokens")
+        
         # OneHotEncode tweet_type
-        ## ToDo: user_id, engaged_user_id, ...
+        ## TODO: user_id, engaged_user_id, ...
         indexer = StringIndexer(inputCol="tweet_type", outputCol="tweet_type_id")
         outputDF = indexer.fit(outputDF).transform(outputDF)
-        
-        #indexer = 
-        
+        #onehot
         encoder = OneHotEncoderEstimator(inputCols=["tweet_type_id"], outputCols=["tweet_type_onehot"])
         model = encoder.fit(outputDF)
         outputDF = model.transform(outputDF)
         # for explainability safe this
         self.explainOneHotDF = outputDF.select("tweet_type", "tweet_type_id", "tweet_type_onehot")
         
-        
+        # tf/idf text_tokens, hashtags, 
         
         
         # might not need
