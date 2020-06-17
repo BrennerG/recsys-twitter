8a66f2cebdf0c0631e64615cfc8e74afa65a01e5
Raphael Schotola <e01225193@c100.local>
2020-06-17 20:17:46 +0200

    implemented gridsearch for random forest

--- src/rforest.py
+++ src/rforest.py
@@ -10,6 +10,7 @@ import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 from twitter_preproc import twitter_preproc
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 
 #spark = SparkSession.builder.appName("ChiSquareSpark").getOrCreate()
 spark = SparkSession \
@@ -25,6 +26,8 @@ train = "///user/pknees/RSC20/training.tsv"
 # the full test file has 12.434.838 lines
 testfile = "///user/pknees/RSC20/val.tsv"
 
+do_crossval = True
+
 preproc = twitter_preproc(spark, sc, train, testFile=testfile)
 
 traindata = preproc.getDF()
@@ -32,12 +35,36 @@ testdata = preproc.getTestDF()
 
 preds = ["like", "retweet", "reply", "retweet_comment"]
 
-for feat in preds:
-    # train a random forest with default vals...
-    rf = RandomForestClassifier(labelCol=feat, featuresCol="all_features", numTrees=20, maxDepth=5, seed=42)
-    model = rf.fit(traindata)
-    pred = model.transform(testdata)
-    pred_out = pred.select("tweet_id","engaging_user_id","probability","prediction")
-    pd.DataFrame(pred_out.collect(),
-                 columns=["tweet_id","engaging_user_id","probability","prediction"])\
-        .to_csv("../output/" + feat + "_out.csv", sep=",")
+if not do_crossval:
+    for feat in preds:
+        # train a random forest with default vals...
+        rf = RandomForestClassifier(labelCol=feat, featuresCol="all_features", numTrees=20, maxDepth=5, seed=42)
+        model = rf.fit(traindata)
+        pred = model.transform(testdata)
+        pred_out = pred.select("tweet_id","engaging_user_id","probability","prediction")
+        pd.DataFrame(pred_out.collect(),
+                     columns=["tweet_id","engaging_user_id","probability","prediction"])\
+            .to_csv("../output/" + feat + "_out.csv", sep=",")
+
+if do_crossval:
+    for feat in preds:
+        # train a random forest with the best parameters from a grid search...
+        evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction", labelCol=feat)
+        evaluator.setMetricName('areaUnderPR') # optimisiing for area under Precision Recall curve - same as used for the recsys challenge
+        rf = RandomForestClassifier(labelCol=feat, featuresCol="all_features", seed=42)
+        paramGrid = (ParamGridBuilder()
+                     .addGrid(rf.numTrees, [5, 20, 50, 100])
+                     .addGrid(rf.maxDepth, [2, 5, 7, 10])
+                     .addGrid(rf.minInstancesPerNode, [1, 2, 3])
+                     .build())
+        crossval = CrossValidator(estimator=rf,
+                                  estimatorParamMaps=paramGrid,
+                                  evaluator=evaluator,
+                                  numFolds=5,
+                                  seed=42)
+        cvModel = crossval.fit(traindata)
+        pred = cvModel.transform(testdata)
+        pred_out = pred.select("tweet_id","engaging_user_id","probability","prediction")
+        pd.DataFrame(pred_out.collect(),
+                     columns=["tweet_id","engaging_user_id","probability","prediction"])\
+            .to_csv("../output/" + feat + "_out.csv", sep=",")
