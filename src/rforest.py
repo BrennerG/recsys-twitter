5a7bc39f793f02a13db18141add13eb683335fdf
oStritze <oliver.stritzel@googlemail.com>
2020-06-30 12:23:59 +0200

    added rf stuff

--- src/rforest.py
+++ src/rforest.py
@@ -22,16 +22,23 @@ sc = spark.sparkContext
 #train = "///tmp/traintweet_1000.tsv"
 #testfile = "///user/e11920598/test_1000.tsv"
 # the full train file has 121.386.431 lines
-train = "///user/pknees/RSC20/training.tsv"
+#train = "///user/pknees/RSC20/training.tsv"
 # the full test file has 12.434.838 lines
-testfile = "///user/pknees/RSC20/val.tsv"
+#testfile = "///user/pknees/RSC20/val.tsv"
+
+### for ensemble:
+train = "///tmp/supersecret_train40k_bootstrap.tsv"
+#train = "///tmp/traintweet_1000.tsv"
+testfile = "///tmp/supersecret_test5k_bootstrap.tsv"
+valfile = "///tmp/supersecret_ensembletrain5k_bootstrap.tsv"
 
 do_crossval = True
 
-preproc = twitter_preproc(spark, sc, train, testFile=testfile)
+preproc = twitter_preproc(spark, sc, train, testFile=testfile, valFile=valfile)
 
 traindata = preproc.getDF()
 testdata = preproc.getTestDF()
+valdata = preproc.getValDF()
 
 preds = ["like", "retweet", "reply", "retweet_comment"]
 
@@ -53,9 +60,9 @@ if do_crossval:
         evaluator.setMetricName('areaUnderPR') # optimisiing for area under Precision Recall curve - same as used for the recsys challenge
         rf = RandomForestClassifier(labelCol=feat, featuresCol="all_features", seed=42)
         paramGrid = (ParamGridBuilder()
-                     .addGrid(rf.numTrees, [5, 20, 50, 100])
-                     .addGrid(rf.maxDepth, [2, 5, 7, 10])
-                     .addGrid(rf.minInstancesPerNode, [1, 2, 3])
+                     .addGrid(rf.numTrees, [20, 50])
+                     .addGrid(rf.maxDepth, [5, 10, 25])
+                     .addGrid(rf.minInstancesPerNode, [1, 3])
                      .build())
         crossval = CrossValidator(estimator=rf,
                                   estimatorParamMaps=paramGrid,
@@ -67,4 +74,11 @@ if do_crossval:
         pred_out = pred.select("tweet_id","engaging_user_id","probability","prediction")
         pd.DataFrame(pred_out.collect(),
                      columns=["tweet_id","engaging_user_id","probability","prediction"])\
-            .to_csv("../output/" + feat + "_out.csv", sep=",")
+            .to_csv("../output/" + "rf_" + feat + "_out_test.csv", sep=",")
+        
+        if 'valfile' in locals():
+            predval = cvModel.transform(valdata)
+            predval_out = predval.select("tweet_id","engaging_user_id","probability","prediction")
+            pd.DataFrame(predval_out.collect(),
+                     columns=["tweet_id","engaging_user_id","probability","prediction"])\
+                .to_csv("../output/" + "rf_" + feat + "_out_ensembletrain.csv", sep=",")
